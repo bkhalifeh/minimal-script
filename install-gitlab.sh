42274242fb307209d6e5d7c34b1f5045d67b5ccc
Behzad Khalifeh <wexsilon2001@protonmail.com>
2024-08-25 00:13:13 +0330

    Update install-gitlab.sh

--- install-gitlab.sh
+++ install-gitlab.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sudo apt-get update
 sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
-sudo apt-get install -y postfix
+# sudo apt-get install -y postfix
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-sudo EXTERNAL_URL="https://gitlab.example.com" apt-get install gitlab-ce
+sudo EXTERNAL_URL="https://$1" apt-get install gitlab-ce
