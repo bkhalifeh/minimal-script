031f156e5ea34d047dcb62ae45d31a38d05aa0f0
Behzad Khalifeh <wexsilon2001@protonmail.com>
2024-08-26 05:20:04 +0330

    Update get-ssl.sh

--- get-ssl.sh
+++ get-ssl.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
-echo "Enter your email:"
-read email
+MARKER_FILE="$HOME/.acme_initialized"
+if [ ! -f "$MARKER_FILE" ]; then
+    echo "Enter your email:"
+    read email
+    curl https://get.acme.sh | sh -s email="$email"
+    ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+    apt-get install socat
+    touch "$MARKER_FILE"
+fi
 echo "Enter your domain:"
 read domain
-curl https://get.acme.sh | sh -s email="$email"
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-apt-get install socat
 ~/.acme.sh/acme.sh --issue -d $domain --standalone
