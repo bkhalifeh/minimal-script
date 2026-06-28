dde112a0198bcac84c309800872c64f7af291c0d
Behzad Khalifeh <wexsilon2001@protonmail.com>
2026-06-28 13:05:17 +0330

    Update add-swap.sh

--- add-swap.sh
+++ add-swap.sh
@@ -3,4 +3,4 @@ sudo fallocate -l 8G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo echo "/swapfile swap swap defaults 0 0" >>‌ /etc/fstab
+echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
